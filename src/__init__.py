66324164bbaa64c928443ddf33dc18845d4413c1
Kon <bokarius@comcast.net>
2015-03-17 17:44:06 -0700

    minor doc changes

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 #    __init__.py
 #    ~~~~~~~~~
-#    This module implements the package init class.
+#    This module implements the package init functionality.
 #    :authors: Justin Karneges, Konstantin Bokarius.
 #    :copyright: (c) 2015 by Fanout, Inc.
 #    :license: MIT, see LICENSE for more details.
