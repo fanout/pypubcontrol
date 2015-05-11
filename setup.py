bc4c138089deccf9d459253f593763ab31f1e7cf
Justin Karneges <justin@affinix.com>
2015-05-11 11:51:35 -0700

    formatting

--- setup.py
+++ setup.py
@@ -4,23 +4,23 @@ from setuptools import setup
 import sys
 
 if sys.version_info >= (3,):
-	install_requires = ["PyJWT>=0,<1","requests>=2,<3","tnetstring3>=0,<1"]
+	install_requires = ['PyJWT>=0,<1','requests>=2,<3','tnetstring3>=0,<1']
 else:
-	install_requires = ["PyJWT>=0,<1","requests>=2,<3","tnetstring>=0,<1"]
+	install_requires = ['PyJWT>=0,<1','requests>=2,<3','tnetstring>=0,<1']
 
 setup(
-name="pubcontrol",
-version="2.2.0",
-description="EPCP library",
-author="Justin Karneges",
-author_email="justin@affinix.com",
-url="https://github.com/fanout/pypubcontrol",
-license="MIT",
-package_dir={'pubcontrol': 'src'},
-packages=['pubcontrol'],
-install_requires=install_requires,
-classifiers=[
-	"Topic :: Utilities",
-	"License :: OSI Approved :: MIT License"
-]
+	name='pubcontrol',
+	version='2.2.0',
+	description='EPCP library',
+	author='Justin Karneges',
+	author_email='justin@affinix.com',
+	url='https://github.com/fanout/pypubcontrol',
+	license='MIT',
+	package_dir={'pubcontrol': 'src'},
+	packages=['pubcontrol'],
+	install_requires=install_requires,
+	classifiers=[
+		'Topic :: Utilities',
+		'License :: OSI Approved :: MIT License'
+	]
 )
