0bbcc44303b42722a960d3a9e4b09a76042bcec5
Justin Karneges <justin@affinix.com>
2015-05-11 11:50:08 -0700

    install correct version of tnetstring

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+import sys
+
+if sys.version_info >= (3,):
+	install_requires = ["PyJWT>=0,<1","requests>=2,<3","tnetstring3>=0,<1"]
+else:
+	install_requires = ["PyJWT>=0,<1","requests>=2,<3","tnetstring>=0,<1"]
 
 setup(
 name="pubcontrol",
@@ -12,7 +18,7 @@ url="https://github.com/fanout/pypubcontrol",
 license="MIT",
 package_dir={'pubcontrol': 'src'},
 packages=['pubcontrol'],
-install_requires=["PyJWT>=0,<1","requests>=2,<3"],
+install_requires=install_requires,
 classifiers=[
 	"Topic :: Utilities",
 	"License :: OSI Approved :: MIT License"
